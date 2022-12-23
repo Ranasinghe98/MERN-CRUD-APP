20679a20f9b66ede1e3b3c6d8379551e1d9dd877
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2022-12-23 23:23:10 +0530

    Deploy config updated

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const connectDb = require('./config/connectDb')
+const path = require('path')
 
 //PORT Config
 const PORT = process.env.PORT || 8000
@@ -41,6 +42,16 @@ app.put('/posts/:id', postRoutes.updatePost)
 
 app.delete('/posts/:id', postRoutes.deletePost)
 
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`)
